0cd1a3e5e28107fb4c9f138b19a3448ef242a0f6
francont924 <fran.contreraass@duocuc.cl>
2026-05-29 23:35:23 -0400

    avance del proyecto: conexion entre productos y eureka

--- src/main/java/cl/paris/Producto/Service/RecService.java
+++ src/main/java/cl/paris/Producto/Service/RecService.java
@@ -1,24 +1,23 @@
 package cl.paris.Producto.Service;
-
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import cl.paris.Producto.model.Producto;
+
 import cl.paris.Producto.Repository.ReProducto;
+import cl.paris.Producto.model.Producto;
 
 @Service
 public class RecService {
     @Autowired
     private ReProducto reProducto;
-    public RecService(ReProducto reProducto){
-        this.reProducto = reProducto;
-         }
-        public List<Producto> getAllProducto() {
-            return reProducto.findAll();
-        }
 
-        public void serById(Producto producto) {
-            reProducto.save(producto);
-        }
+    public List<Producto> getAllProducto() {
+        return reProducto.findAll();
+    }
+
+    public void serById(Producto producto) {
+        reProducto.save(producto);
+    }
     
 }
